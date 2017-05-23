e9c5e4592ae127e16a92008d238bd1d94e28c2e2
ketan <code.ketan@gmail.com>
2017-05-23 12:19:22 +0530

    pushing fish market app

---
+++ fishmarket-app/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyDPufAGIYJXW6kCCg8hi_B6LUr6DmG6xFU",
+  authDomain: "fish-market-react-app.firebaseapp.com",
+  databaseURL: "https://fish-market-react-app.firebaseio.com",
+});
+
+export default base;
---
+++ fishmarket-app/src/components/App.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
+import base from '../base';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+    this.updateFish = this.updateFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
+    this.removeOrder = this.removeOrder.bind(this);
+
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+      , {
+        context: this,
+        state:'fishes'
+      });
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+    if (localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`,
+      JSON.stringify(nextState.order));
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  addFish(fish) {
+    const fishes = {...this.state.fishes};
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    this.setState({ fishes });
+  }
+
+  removeFish(key) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null;
+    this.setState({fishes});
+  }
+
+  removeOrder(key) {
+    const order = {...this.state.order};
+    delete order[key];
+    this.setState({order});
+  }
+
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({fishes});
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
+  addToOrder(key) {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({order});
+  }
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} index={key} addToOrder={this.addToOrder} details={this.state.fishes[key]} />)
+            }
+          </ul>
+        </div>
+        <Order fishes={this.state.fishes} order={this.state.order} params={this.props.params} removeOrder={this.removeOrder} />
+        <Inventory 
+          addFish={this.addFish} 
+          loadSamples={this.loadSamples} 
+          fishes={this.state.fishes} 
+          updateFish={this.updateFish} 
+          removeFish={this.removeFish}
+          storeId={this.props.params.storeId} 
+        />
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  params: React.PropTypes.object.isRequired
+}
+
+export default App;
